46de82badecdda9e3b5237de77603c95c5fd5403
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-10-24 07:08:32 +0200

    reverted to only using prompts

--- scripts/exercise-one.js
+++ scripts/exercise-one.js
@@ -7,17 +7,15 @@ function guessNumberGame(){
     const randomNumber = Math.floor((Math.random() * 10) + 1);
     console.log(randomNumber)
     let promptNumber = Number(myPrompt)
-    
     while(promptNumber !== randomNumber){
         if(promptNumber > randomNumber){
             myPrompt = prompt("thats too high try again")
         }else if (promptNumber < randomNumber){
             myPrompt = prompt("thats too low try again");
         }
-        
         promptNumber = Number(myPrompt); 
    }
-   console.log("Wow that is correct");
+   alert(`Nice that is correct i was thining of ${promptNumber}`)
 }
 
 
